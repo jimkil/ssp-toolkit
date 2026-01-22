85308f3655c2d978047544baa70d6877dc249c39
Tom Camp <tom.camp@civicactions.com>
2026-01-22 10:51:55 -0500

    Updating export-to command.

--- src/tools/cli.py
+++ src/tools/cli.py
@@ -8,6 +8,7 @@ from loguru import logger
 
 from tools.create_files import create_files_cmd
 from tools.create_project import create_project_cmd
+from tools.export_to import export_to_cmd
 from tools.load_project import load_project_cmd
 from tools.logging_config import setup_logging
 from tools.make_families import make_families_cmd
@@ -26,6 +27,7 @@ def cli():
 cli.add_command(create_project_cmd)
 cli.add_command(load_project_cmd)
 cli.add_command(create_files_cmd)
+cli.add_command(export_to_cmd)
 cli.add_command(make_families_cmd)
 cli.add_command(make_ssp_cmd)
 cli.add_command(sop_cmd)
